8f67add92c5bd1a4d3d0effd32a5ab97043fc219
Tushar <tushar.nyc20@gmail.com>
2025-08-20 20:56:04 +0530

    update input area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,46 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("");
+  const [summary, setSummary] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
 
-  const[text, setText] = useState("")
+  const handleSumbit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
 
-  const handleSumbit = async (e: React.FormEvent) => {};
+    try {
+      const response = await fetch("/myendpont", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ text }),
+      });
+
+      if (!response.ok) throw new Error("Request Failed");
+
+      const data = await response.json();
+      setSummary(data);
+    } catch (error) {
+      console.log("Error in sending data", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <div className="flex  ">
-      <form className="flex items-center" onSubmit={handleSumbit}>
-        <input
-          type="text"
+    <div className="p-6 max-w-2xl mx-auto">
+      <form onSubmit={handleSumbit} className="space y-4">
+        <textarea
           value={text}
           onChange={(e) => setText(e.target.value)}
-          placeholder="The text or details you want to translate"
+          placeholder="Paste privacy/terms here..."
+          className="w-full h-50 p-4 border rounded-md"
           required
         />
-        <button className="bg-emerald-500 hover:bg-emerald-800">Translate</button>
+        <button type="submit" className= "px-4 py-2 rounded bg-emerald-500 hover:bg-emerald-800">
+          Translate
+        </button>
       </form>
     </div>
   );
